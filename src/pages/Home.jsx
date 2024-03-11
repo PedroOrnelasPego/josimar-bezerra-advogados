cff0d621113f249fd65878996ae88550511abd96
unknown <pedroornelaspego@gmail.com>
2024-03-11 02:28:43 -0300

    chore: criação do componente de about

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import About from "../components/About";
 import Header from "../components/Header";
 import "./Home.scss";
 
@@ -5,6 +6,7 @@ const Home = () => {
   return (
     <div className="home">
       <Header />
+      <About />
     </div>
   );
 };
